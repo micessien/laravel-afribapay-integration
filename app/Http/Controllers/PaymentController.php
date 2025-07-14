d931042f86031ddfc8b9a6737ee5d4b0e69d3483
micessien <micaeldie@gmail.com>
2025-07-14 21:59:14 +0000

    Generate access token and save it in database for request

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
+use App\Models\AfribapayAccessToken;
 
 class PaymentController extends Controller
 {
@@ -31,11 +33,86 @@ class PaymentController extends Controller
             'amount' => 'required|numeric|min:100',
         ]);
 
-        dd($request);
+        // Get Access Token
+        $token = $this->get_accesstoken();
+        
+        dd($token);
     }
 
+    /**
+     * Get the Access Token
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response $token
+     */
+    public function get_accesstoken()
+    {
+        $AccessTokenData = AfribapayAccessToken::where('active', true)->first();
+        if ($AccessTokenData) {
+            $AccessTokenEnd = Carbon::parse($AccessTokenData->expires_at);
+            // Access Token has expired
+            if ($AccessTokenEnd->isPast()) {
+                // Generate New Access Token
+                $tokenData = $this->generate_accesstoken();
+                // Make actual Access Token Inactive
+                $AccessTokenData->active = false;
+                $AccessTokenData->save();
+                
+                // Create New token and save
+                $dateWith20Hours = Carbon::now()->addHours(20);
+                $newAccessToken = new AfribapayAccessToken;
+                $newAccessToken->token =  $tokenData->data->access_token;
+                $newAccessToken->expires_at =  $dateWith20Hours;
+                $newAccessToken->save();
+                // Return Access Token
+                return $tokenData->data->access_token;
+            }
+
+            // Access Token has not expired
+            $token = $AccessTokenData->token;
+            return $token;
+        }
+
+        // Generate New Access Token
+        $tokenData = $this->generate_accesstoken();
+        // Create New token and save
+        $dateWith20Hours = Carbon::now()->addHours(20);
+        $newAccessToken = new AfribapayAccessToken;
+        $newAccessToken->token =  $tokenData->data->access_token;
+        $newAccessToken->expires_at =  $dateWith20Hours;
+        $newAccessToken->save();
+        // Return Access Token
+        return $tokenData->data->access_token;
+    }
+    
+    /**
+     * Generate the Access Token from API
+     *
+     * @return \Illuminate\Response $response
+     */
     public function generate_accesstoken()
     {
-        
+        $url = env("AFRIBAPAY_API_URL")."/v1/token";
+
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json',
+                'Authorization: Basic '.base64_encode(env('AFRIBAPAY_API_USER').':'.env('AFRIBAPAY_API_KEY')) //  Api_user and Api_key from your merchand account
+            ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        return json_decode($response);
     }
 }
---
+++ app/Models/AfribapayAccessToken.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AfribapayAccessToken extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $dates = ['expires_at'];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'token',
+        'active',
+    ];
+    
+}
